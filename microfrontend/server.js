5a635d32a3c38b10c74466e06667f90a8258d9fa
mwieser <manuel.wieser@karriere.at>
2019-03-22 12:40:48 +0100

    Use environment variables for everything

--- microfrontend/server.js
+++ microfrontend/server.js
@@ -12,6 +12,6 @@ app.get('*', function (req, res) {
 });
 
 app.listen(3000, () => {
-    console.log(`Serving microfrontend at http://localhost:3000`);
-    console.log(`Serving microfrontend at http://localhost:3000/?vm=%7B%22username%22%3A%22Manuel%22%7D`)
+    console.log(`Serving microfrontend at ${process.env.URL}`);
+    console.log(`Serving microfrontend at ${process.env.URL}/?vm=%7B%22username%22%3A%22Manuel%22%7D`)
 });
